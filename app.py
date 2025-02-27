a550d93c3e81a2b8c3538d93b7a176f2a8786c9b
just-ruggles <ammoncadal@eafit.edu.co>
2025-02-27 09:57:08 -0500

    app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ st.title("Mi primera app")
 
 st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
 st.write("facilmente puedo realizar backend y frontend:")
-image = Image.open("gato.jpg")
+image = Image.open("spider-cat.jpg")
 
-st.image(image, caption="GATO")
+st.image(image, caption="MILES-MEWRALES")
 
 
 texto = st.text_input("Escribe algo", "Este es mi texto")
