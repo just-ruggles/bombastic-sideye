353a3b10bd64148647b758d9c329b3ec2dea7f45
just-ruggles <ammoncadal@eafit.edu.co>
2025-04-22 15:37:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,45 @@
 import streamlit as st
 from PIL import Image
 
-st.title("Mi primera app")
+st.title("Mi primera app (Peter Parker estaría orgulloso)")
 
-st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales. O para salvar al mundo, FNSM app en camino.")
 st.write("facilmente puedo realizar backend y frontend:")
 image = Image.open("spider-cat.jpg")
 
 st.image(image, caption="MILES-MEWRALES")
 
 
-texto = st.text_input("Escribe algo", "Este es mi texto")
+texto = st.text_input("Escribe algo", "Miles Morales es el mejor Spider-man")
 st.write("Texto ingresado:", texto)
 
 st.subheader("Ahora usemos 2 columnas")
 col1, col2 = st.columns(2)
 with col1:
   st.subheader("Esta es la primera columna")
-  st.write("Las interfaces multimodales mejoran la experiencia del usuario")
+  st.write("Spider-man es un amigo y vecino de la gente :)")
   resp = st.checkbox("Estoy de acuerdo")
+  resp_2 = st.checkbox("Por supuesto que sí")
+  resp_3 = st.checkbox("NO")
   if resp:
     st.write("Correcto")
+  if resp_2:
+    st.write("<3")
+  if resp_3:
+    st.write("JJ. Jameson sé que eres tú -.-")
 with col2:
   st.subheader("Esta es la segunda columna")
   modo = st.radio("Que modalidad es la principal en tu interfaz", ("Visual","Auditiva", "Tactil"))
   if modo == "Visual":
-    st.write("La vista es fundamental para tu interfaz")
+    st.write("La vista es fundamental para ver cuantas telarañas debes lanzar")
   if modo == "Auditiva":
-    st.write("La audicion es fundamental para tu interfaz")
+    st.write("La audicion es fundamental para saber cuanta gente hay basándote en las voces")
   if modo == "Tactil":
-    st.write("El tacto es fundamental para tu interfaz")
+    st.write("El tacto es fundamental para derrotar a los malos (como a Scoprion...)")
 
 st.subheader("Uso de Botones")
 if st.button("Presiona el botón"):
-      st.write("Gracias por presionar")
+      st.write("¿Presionarías todo lo que te dicen que toques? ¿y si fuera una bomba?")
 else:
   st.write("No has presionado eso")
 
